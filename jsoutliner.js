1bbfbd6281f2cba9a623575d64b6ea813559b0b0
Mark Macdonald <mamacdon@ca.ibm.com>
2011-09-06 11:41:09 -0400

    License header

--- jsoutliner.js
+++ jsoutliner.js
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials are made 
+ * available under the terms of the Eclipse Distribution License v1.0 
+ * (http://www.eclipse.org/org/documents/edl-v10.html).
+ * 
+ * Contributors: IBM Corporation - initial API and implementation
+ ******************************************************************************/
+
 /*jslint debug:true*/
 /*global console define*/
 define(["uglify-js", "transformjs"], function(mUglifyJs, mTransformJs) {
